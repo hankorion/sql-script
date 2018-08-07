c64d25c8b9a7717e37de44af963c921180a623f9
hankorion <hankzhang.orion@gmail.com>
2018-08-07 19:05:57 +0800

    add author script

---
+++ SW-02-AlterTables/author.sql
@@ -0,0 +1,48 @@
+-- ##################################################
+--
+--	Script to maintain the table schema
+-- 	2018 Copyright (c) SG Win
+--	Script v. 0.0.2
+--  v. 0.0.1 - init via JIRA DBA-001
+--  v. 0.0.2 - update to reformat the script via JIRA DBA-002
+-- ##################################################
+
+CLEAR SCREEN;
+PROMPT ----------------------------------------------;
+PROMPT SW-02-AlterTables - author.sql start
+PROMPT ----------------------------------------------;
+PROMPT ;
+
+
+-- ##################################################
+PROMPT ;
+PROMPT ----------------------------------------------;
+PROMPT Key notes for this script
+PROMPT ----------------------------------------------;
+PROMPT ;
+
+-- Clean constraints then drop table
+ALTER TABLE
+  author DROP CONSTRAINT PK_AUTHOR_A_ID;
+DELETE FROM
+  author;DROP TABLE author CASCADE CONSTRAINTS;create table author (
+    a_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    a_name VARCHAR(30) NOT NULL
+  );
+
+
+-- Add PK constraint for table author
+ALTER TABLE
+  author
+ADD
+  CONSTRAINT PK_AUTHOR_A_ID PRIMARY KEY (a_id);
+
+-- ####################################################################################################
+
+PROMPT ----------------------------------------------;
+PROMPT SW-02-AlterTables - author.sql end
+PROMPT ----------------------------------------------;
+PROMPT ;
+
+COMMIT;
+
