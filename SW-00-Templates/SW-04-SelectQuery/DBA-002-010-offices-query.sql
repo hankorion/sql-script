e6a4dd7630a6337ee404d8f2e5ab90a8f770da41
hankorion <hankzhang.orion@gmail.com>
2018-08-10 15:20:23 +0800

    restructure folders

---
+++ SW-00-Templates/SW-04-SelectQuery/DBA-002-010-offices-query.sql
@@ -0,0 +1,10 @@
+SELECT `offices`.`officeCode`,
+    `offices`.`city`,
+    `offices`.`phone`,
+    `offices`.`addressLine1`,
+    `offices`.`addressLine2`,
+    `offices`.`state`,
+    `offices`.`country`,
+    `offices`.`postalCode`,
+    `offices`.`territory`
+FROM `classicmodels`.`offices`;
