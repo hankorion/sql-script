3c23fddfeea9529eec46830627b4473f925c4253
hankorion <hankzhang.orion@gmail.com>
2018-08-13 17:12:04 +0800

    add insert script to add Singapore office

---
+++ SW-99-patches/DBA-010/SW-03-SequencesTriggersViewsInsertSelect/R01-offices-insert city.sql
@@ -0,0 +1,5 @@
+select * from offices where officeCode = "8";
+
+INSERT INTO `offices` (`officeCode`,`city`,`phone`,`addressLine1`,`addressLine2`,`state`,`country`,`postalCode`,`territory`) VALUES ('8','Singapore','+65 6666 8888','abc road','Level 17',NULL,'SG','006688','APAC');
+
+select * from offices where officeCode = "8";
---
+++ SW-99-patches/DBA-010/SW-04-SelectQuery/R01-office-select top5.sql
@@ -0,0 +1,1 @@
+select * from offices order by officeCode limit 5;
